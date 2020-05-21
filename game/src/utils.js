5c996f6bb0343667b46e8855d1ca095c3a73058e
John Oram <john@oram.ca>
2020-05-20 21:21:59 -0700

    kobold starts at -5,-5

--- game/src/utils.js
+++ game/src/utils.js
@@ -1,20 +1,20 @@
 function hostname() {
     let hostname = window.location.href.split("/")[2]
     if(hostname==="localhost:3000"){
-        return "localhost:2303"
+        return "localhost:2305"
     }
     return hostname
 }
 
 function ws_prefix() {
-    if(hostname()==="localhost:2303"){
+    if(hostname()==="localhost:2305"){
         return "ws"
     }
     return "wss"
 }
 
 function http_prefix() {
-    if(hostname()==="localhost:2303"){
+    if(hostname()==="localhost:2305"){
         return "http"
     }
     return "https"
